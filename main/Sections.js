ed8aa4390b1c601b09cadff7b8d95f2bed4ff656
meowseal <hotdogtoiletcleaner@gmail.com>
2025-10-15 12:12:56 -0500

    Update Sections.js

--- main/Sections.js
+++ main/Sections.js
@@ -7,3 +7,139 @@ Sections = {
     "Files": "NaN",
     "Quit": "NaN",
 };
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
+
+Sections = {
+    "Help": {"Commands": "This is Help/Commands",
+     "About":"This is a simple Operating System that works in a Python Emulator"},
+    "Commands": "NaN",
+    "About": "NaN",
+    "Apps": "NaN",
+    "Files": "NaN",
+    "Quit": "NaN",
+};
